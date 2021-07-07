29511bbc0f0f9e3c1a2b95fd89c5c103cac3ec55
Anton Schubert <anton.schubert@riedel.net>
2021-07-07 13:20:03 +0200

    add binding for srt_connect_callback, replace extern declarations with shared header

--- callback_c.go
+++ callback_c.go
@@ -2,13 +2,16 @@ package srtgo
 
 /*
 #cgo LDFLAGS: -lsrt
-#include <srt/srt.h>
+#include "callback.h"
 
-extern void srtListenCBWrapper(void* opaque, SRTSOCKET ns, int hs_version, struct sockaddr* peeraddr, char* streamid);
-
-void srtListenCB(void* opaque, SRTSOCKET ns, int hs_version, const struct sockaddr* peeraddr, const char* streamid)
+int srtListenCB(void* opaque, SRTSOCKET ns, int hs_version, const struct sockaddr* peeraddr, const char* streamid)
 {
-	srtListenCBWrapper(opaque, ns, hs_version, (struct sockaddr*)peeraddr, (char*)streamid);
+	return srtListenCBWrapper(opaque, ns, hs_version, (struct sockaddr*)peeraddr, (char*)streamid);
+}
+
+void srtConnectCB(void* opaque, SRTSOCKET ns, int errorcode, const struct sockaddr* peeraddr, int token)
+{
+	srtConnectCBWrapper(opaque, ns, errorcode, (struct sockaddr*)peeraddr, token);
 }
 */
 import "C"
