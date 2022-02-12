aa00e51bd9a4cb62007a7458e4221da0e6580a61
Anton Schubert <iSchluff@users.noreply.github.com>
2022-02-12 22:33:02 +0100

    add Read/Write Benchmarks (#44)

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/mattn/go-pointer v0.0.1
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/sys v0.0.0-20200926100807-9d91bd62050c
 )
---
+++ rw_test.go
@@ -0,0 +1,121 @@
+package srtgo
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+type tester struct {
+	blocking string          // blocking state
+	ctx      context.Context // done notification for senders
+	cancel   func()          // cancel tester
+	err      chan error      // error output for senders/receivers
+	started  sync.WaitGroup  // sync goroutines on start
+}
+
+// Creates a connector socket and keeps writing to it
+func (t *tester) send(port uint16) {
+	buf := make([]byte, 1316)
+	sock := NewSrtSocket("127.0.0.1", port, map[string]string{"blocking": t.blocking, "mode": "caller", "transtype": "file"})
+	if sock == nil {
+		t.err <- errors.New("failed to create caller socket")
+		return
+	}
+	defer sock.Close()
+
+	// Wait until all sockets are ready
+	t.started.Done()
+	t.started.Wait()
+
+	err := sock.Connect()
+	if err != nil {
+		t.err <- errors.Wrap(err, "connect")
+		return
+	}
+	for {
+		select {
+		case <-t.ctx.Done():
+			return
+		default:
+		}
+		_, err := sock.Write(buf)
+		if err != nil {
+			t.err <- errors.Wrap(err, "write")
+			return
+		}
+	}
+}
+
+// Creates a listener socket and keeps reading from it
+func (t *tester) receive(port uint16, iterations int) {
+	sock := NewSrtSocket("127.0.0.1", port, map[string]string{"blocking": t.blocking, "mode": "listener", "rcvbuf": "22937600", "transtype": "file"})
+	if sock == nil {
+		t.err <- errors.New("failed to create listener socket")
+		return
+	}
+	buf := make([]byte, 1316)
+	defer sock.Close()
+	defer t.cancel()
+
+	// Wait until all sockets are ready
+	t.started.Done()
+	t.started.Wait()
+
+	err := sock.Listen(1)
+	if err != nil {
+		t.err <- errors.Wrap(err, "listen")
+		return
+	}
+	remote, _, err := sock.Accept()
+	if err != nil {
+		t.err <- errors.Wrap(err, "accept")
+		return
+	}
+	for i := 0; i < iterations; i++ {
+		_, err := remote.Read(buf)
+		if err != nil {
+			t.err <- errors.Wrap(err, "read")
+			return
+		}
+	}
+}
+
+func runTransmitBench(b *testing.B, blocking bool) {
+	blockStr := "0"
+	if blocking {
+		blockStr = "1"
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	t := &tester{
+		ctx:      ctx,
+		blocking: blockStr,
+		cancel:   cancel,
+		err:      make(chan error, 1),
+	}
+	t.started.Add(2)
+	port := randomPort()
+	go t.receive(port, b.N)
+	go t.send(port)
+
+	t.started.Wait()
+	b.ResetTimer()
+	select {
+	case <-ctx.Done():
+		return
+	case err := <-t.err:
+		b.Error(err)
+	}
+}
+
+func BenchmarkRWBlocking(b *testing.B) {
+	runTransmitBench(b, true)
+}
+
+func BenchmarkRWNonBlocking(b *testing.B) {
+	runTransmitBench(b, false)
+}
